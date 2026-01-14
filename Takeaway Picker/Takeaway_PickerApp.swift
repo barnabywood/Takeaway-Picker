e0364d6a8001a20575c23d47d04ee77440a80e9d
barnabywood <barnaby.wood@yahoo.com>
2026-01-14 12:44:38 +0000

    Initial Commit

---
+++ Takeaway Picker/Takeaway_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Takeaway_PickerApp.swift
+//  Takeaway Picker
+//
+//  Created by Barnaby Wood on 14/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Takeaway_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
